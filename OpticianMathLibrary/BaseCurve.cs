015cbf37091e76f38f0ce6380272262f94f4b79f
rmnrdi <robmopti@gmail.com>
2021-01-05 18:38:09 -0500

    Removed TESTER project, unnecessary using statements. Sorted Using statements

--- OpticianMathLibrary/BaseCurve.cs
+++ OpticianMathLibrary/BaseCurve.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace OpticianMathLibrary
+﻿namespace OpticianMathLibrary
 {
     /// <summary>
     /// Base Curve Calculations
